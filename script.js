cc8e7e35b4f1580ede9db5dd600e42d362f9bdd5
LembheVaishnavi <123371870+LembheVaishnavi@users.noreply.github.com>
2023-04-03 22:10:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function clearScreen() {
-    document.getElementById("result").value = "";
-    }
-    
-    // This function display values
-    function display(value) {
-        document.getElementById("result").value += value;
-    }
-    
-    // This function evaluates the expression and returns result
-    function calculate() {
-        var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-    }
+const celsiusElem = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusElem.innerHTML = "";
+});
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+
+if (tempType.value === "fahrenheit") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+  } else if (tempType.value === "kelvin") {
+    const KelvinToCelsius = inputValue - 273.15;
+    celsiusElem.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+  }
+}
